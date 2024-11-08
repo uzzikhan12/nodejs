d51886ec30131d714f925e020064a132840a73ae
uzzikhan12 <hk33636625@gmail.com>
2024-11-09 04:06:14 +0500

    api

--- routes/authroute.js
+++ routes/authroute.js
@@ -4,6 +4,5 @@ const Route = express.Router();
 
 Route.post("/login", AuthController.login);
 Route.post("/signup", AuthController.signup);
-Route.get("/checkauth", AuthController.checkauth);
 
 module.exports = Route;
